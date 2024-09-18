93974224a1bc18dd9207b06578f44c521f5d781c
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-09-18 20:32:26 -0300

    adding first components

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <section className="navbar-logo-box">Bruna Boeger</section>
+        <section className="navbar-actions-box">
+          <button>Tema</button>
+          <button>Contato</button>
+        </section>
+      </div>
+    </nav>
+  );
+}
