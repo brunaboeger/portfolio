775285c3383b91e1dc01db7843aa04d43f764a1f
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-09-24 15:13:55 -0300

    adding components scss files and navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
-export default function Navbar() {
+import Button from "../button/Button";
+
+export default function Navbar({ children }) {
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <section className="navbar-logo-box">Bruna Boeger</section>
-        <section className="navbar-actions-box">
-          <button>Tema</button>
-          <button>Contato</button>
-        </section>
+        <section className="navbar-actions-box">{children}</section>
       </div>
+      <aside className="navbar-floating-menu">
+        <Button text="Projetos" />
+        <Button text="Experiências" />
+        <Button text="Habilidades" />
+        <Button text="Aprendizados" />
+        <Button text="Contato" />
+      </aside>
+      <aside className="navbar-floating-menu is-right">
+        <Button text="Esconder grid" />
+      </aside>
     </nav>
   );
 }
