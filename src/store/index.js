6bb269184d2598a68bb808fabf121e718c127c75
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-10-12 17:10:21 -0300

    adding store files

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import pages from "./pages.json";
+import tools from "./tools.json";
+
+import projects from "./projects.json";
+import experiences from "./experiences.json";
+import abilities from "./abilities.json";
+import knowledge from "./knowledge.json";
+import contact from "./contact.json";
+
+const store = {
+  pages,
+  tools,
+  projects,
+  experiences,
+  abilities,
+  knowledge,
+  contact,
+};
+
+export default store;
