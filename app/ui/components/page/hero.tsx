d906c60ccdc55e14c60f9376e5d101bf051a9777
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-02-12 10:21:10 -0300

    rewrintg heros textos

--- app/ui/components/page/hero.tsx
+++ app/ui/components/page/hero.tsx
@@ -8,7 +8,8 @@ export default function Hero() {
     <>
       <section
         id="banner"
-        className="p-container-lr container-sm flex-column justify-center align-center text-center h-100vh z-index-20"
+        className="p-container-lr flex-column justify-center align-center text-center h-100vh z-index-20"
+        style={{ maxWidth: "54rem", margin: "auto" }}
       >
         <p className="paragraph">{heroTexts.pretitle}</p>
         <h1 className={`${font.heading} mtb-5 w-100`}>{heroTexts.title}</h1>
