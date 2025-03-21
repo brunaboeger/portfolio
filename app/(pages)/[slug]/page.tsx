cee7e17f8874b24b1e9bc1dbc23d24a6c53907a5
Bruna <53488109+brunaboeger@users.noreply.github.com>
2025-03-21 11:15:45 -0300

    feat: add get-all-projects function

---
+++ app/(pages)/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return (<h1>Página</h1>);
+}
+
+export default Page;
--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,21 +1,21 @@
-"use client";
+import Link from "next/link";
+import { getAllProjects } from "@/app/data/get-all-projects";
 
-import Navbar from "@/app/ui/components/page/navbar";
-import Hero from "@/app/ui/components/page/hero";
-import Projects from "@/app/ui/components/page/projects";
-import Profile from "@/app/ui/components/page/profile/profile";
-import Footer from "@/app/ui/components/page/footer";
-import UserDayTime from "@/app/ui/components/daytime";
+const HomePage = async () => {
+  const projects = await getAllProjects();
 
-export default function Page() {
+  if (!projects) return null;
+
   return (
     <div className="container gap-5 position-relative">
-      <Navbar />
-      <Hero />
-      <Projects />
-      <Profile />
-      <Footer />
-      <UserDayTime />
+      {/* Temporário */}
+      <div className="p-5 flex flex-column gap-4">
+        {projects.map((project) => (
+          <Link key={project.id} href={project.slug}>{project.name}</Link>
+        ))}
+      </div>
     </div>
   );
 }
+
+export default HomePage;
